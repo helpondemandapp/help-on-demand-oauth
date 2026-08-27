ab09e90f2c933952499cc15da07ba5e3595f72c6
JoseRodriguez-Rivas <jose.rivas@helpondemand.com>
2026-08-27 13:51:34 -0700

    Add event body parsing and header context logging in API handler

--- lambda-functions/src/functions/oauth-api/register/index.ts
+++ lambda-functions/src/functions/oauth-api/register/index.ts
@@ -4,7 +4,8 @@ import { setContext } from '/opt/nodejs/logging/wideEvent.js';
 export const handler = apiRequestLambdaWrapper({
   callback: async (event) => {
     const res = new ResponseBuilder();
-    setContext('eventBody', event.body ?? '');
+    setContext('eventBody', JSON.parse(event.body ?? '{}'));
+    setContext('eventHeaders', event.headers);
     return res.status(200).json({ message: 'Hello world!' });
   },
 });
